b53f6c9fedb4dba8f6c56490c090fb1134638c0c
puran kumar <mylarmpuran@gmail.com>
2025-06-16 18:02:58 +0530

    "transaction"

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
 import Products from "scenes/products";
 import Customers from "scenes/customers"
-
+import Transactions from "scenes/transactions"
 
 function App() {
   const mode = useSelector((state) => state.global.mode)
@@ -24,14 +24,14 @@ function App() {
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
               <Route path="/customers" element={<Customers />} />
-              {/* <Route path="/transactions" element={<Transactions />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/overview" element={<Overview />} />
-              <Route path="/daily" element={<Daily />} />
-              <Route path="/monthly" element={<Monthly />} />
-              <Route path="/breakdown" element={<Breakdown />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/performance" element={<Performance />} /> */}
+              <Route path="/transactions" element={<Transactions />} />
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/overview" element={<Overview />} /> */}
+              {/* <Route path="/daily" element={<Daily />} /> */}
+              {/* <Route path="/monthly" element={<Monthly />} /> */}
+              {/* <Route path="/breakdown" element={<Breakdown />} /> */}
+              {/* <Route path="/admin" element={<Admin />} /> */}
+              {/* <Route path="/performance" element={<Performance />} /> */}
             </Route>
           </Routes>
         </ThemeProvider>
