3d5f669b7e1a2200fda0aead937ab7e7ee9275aa
mylarmpuran <mylarmpuran@gmail.com>
2026-01-15 00:42:12 +0530

    "complete overview"

--- client/src/scenes/overview/index.js
+++ client/src/scenes/overview/index.js
@@ -1,29 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormControl, MenuItem, InputLabel, Box, Select } from '@mui/material';
 import Header from "components/Header"
-import OverViewChart from "components/OverviewChart"
+import OverViewChart from "components/OverViewChart"
 
 const Overview = () => {
     const [view, setView] = useState("units");
 
     return (
-        <Box>
-            <Header title="OVERVIEW"
-                subtitle="Overview of general revenue and profit" />
-            <Box>
-                <FormControl>
-                    <InputLabel>View</InputLabel>
-                    <Select
-                        value={view}
-                        lable="View"
-                        onChange={(e) => setView(e.target.value)}>
-                        <MenuItem value="sales">Sales</MenuItem>
-                        <MenuItem value="units">Units</MenuItem>
-                    </Select>
-                </FormControl>
-                <OverViewChart view={view}/>
-            </Box>
-        </Box>
+       <Box m="1.5rem 2.5rem">
+      <Header
+        title="OVERVIEW"
+        subtitle="Overview of general revenue and profit"
+      />
+      <Box height="75vh">
+        <FormControl sx={{ mt: "1rem" }}>
+          <InputLabel>View</InputLabel>
+          <Select
+            value={view}
+            label="View"
+            onChange={(e) => setView(e.target.value)}
+          >
+            <MenuItem value="sales">Sales</MenuItem>
+            <MenuItem value="units">Units</MenuItem>
+          </Select>
+        </FormControl>
+        <OverViewChart view={view} />
+      </Box>
+    </Box>
     )
 }
 
